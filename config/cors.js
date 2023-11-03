b99f54d8910007dcaf81f85d7a3d42130e1192c2
Martina <martina.fatato@gmail.com>
2023-11-03 14:33:17 +0000

    (feat): Added cors whitelist

---
+++ config/cors.js
@@ -0,0 +1,15 @@
+const allowedOrigins = require('./allowedOrigins');
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    console.log("origin", origin);
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+
+    }
+  }
+}
+
+module.exports = corsOptions;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log("Starting backend!");
 require('dotenv');
 const express = require('express');
 const cors = require('cors');
+const corsOptions = require('./config/cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -9,10 +10,19 @@ const app = express();
 const PORT = 2500;
 
 //middleware - need to research what these do...
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send("Got base route");
+})
+
+app.get('/users', (req, res) => {
+  res.send("Users!!");
+})
 
 
 app.listen(PORT, () => {

4be0f1ff84bcdc4f8a166acf3971ab41fa73a434
Martina <martina.fatato@gmail.com>
2024-02-29 15:43:34 +0000

    (test): Test headers for cookies

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -6,6 +6,8 @@ const credentials = (req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin)
     res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With, Set-Cookie, Cookie, Bearer');
   }
 
   next();
